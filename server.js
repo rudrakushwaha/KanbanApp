29ebb41da488c1bfc8e0ca0c5830b0a716d21b41
rudrakshkushwaha <rudrakshkushwaha907@gmail.com>
2023-09-07 13:41:51 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,108 @@
+// import express from "express";
+// import bodyParser from "body-parser";
+// import axios from "axios";
+const bodyParser=require("body-parser")
+const express=require("express")
+const axios=require("axios")
+const mongoose = require('mongoose');
+
+
+
+const app = express();
+const port = 3000;
+const API_URL = "http://localhost:4000";
+// const dotenv= require("dotenv")   //requiring dotenv
+
+
+//including or using dotenv to secure our necessary or important data
+// dotenv.config({path:"./config.env"})
+
+// require("./db/connection")
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Route to render the main page
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts`);
+    // console.log(response);
+    res.render("index.ejs", { posts: response.data });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching posts" });
+  }
+});
+
+// Route to render the edit page
+app.get("/new/:id", (req, res) => {
+  console.log(req.params.id)
+  // console.log(id)
+  res.render("modify.ejs", { heading: "New Post", submit: "Create Post",id:req.params.id });
+});
+
+app.get("/edit/:id", async (req, res) => {
+  try {
+    console.log(req.params.id)
+    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+    
+    console.log(response.data);
+    res.render("modify.ejs", {
+      heading: "Edit Post",
+      submit: "Update Post",
+      post: response.data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching post" });
+  }
+});
+
+// Create a new post
+app.post("/api/posts", async (req, res) => {
+  try {
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+    // console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error creating post" });
+  }
+});
+
+// Partially update a post
+app.post("/api/posts/:id", async (req, res) => {
+  console.log("called");
+  try {
+    const response = await axios.patch(
+      `${API_URL}/posts/${req.params.id}`,
+      req.body
+    );
+    // console.log(response.data)
+    // console.log(response.data.columnId)
+    // console.log("helloies")
+    // console.log(response.data);
+    // console.log("helloies")
+    // res.redirect("/");F
+    // const uppost=
+    // res.render("index.ejs", { posts: response.data });
+    res.redirect("/")
+
+  } catch (error) {
+    res.status(500).json({ message: "Error updating post" });
+  }
+});
+
+// Delete a post
+app.get("/api/posts/delete/:id", async (req, res) => {
+  try {
+    await axios.delete(`${API_URL}/posts/${req.params.id}`);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting post" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend server is running on http://localhost:${port}`);
+});
